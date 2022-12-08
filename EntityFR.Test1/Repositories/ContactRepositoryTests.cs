139c0dc2c61115b06ecda930e0cbbca7c9d86995
Amadou Dioubate <dioubateamadou11@gmail.com>
2022-12-08 17:07:20 +0100

    Test unitaires service 1

--- EntityFR.Test1/Repositories/ContactRepositoryTests.cs
+++ EntityFR.Test1/Repositories/ContactRepositoryTests.cs
@@ -1,14 +1,128 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net;
+using System.Runtime.Remoting.Contexts;
+using EntityFR.Repositories;
+using EntityFR.Models;
 
 namespace EntityFR.Test1.Repositories
 {
     [TestClass]
-    public class UnitTest1
+    public class ContactRepositoryTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        /*
+         * ****** Ne pas utiliser la base de données de la prod - prévoir une BD de test
+         * - Installer Entity Framework 6
+         * - Dans App.config, via ConnectionString, fournir la BD de test.
+         * Le context du Repository, va utiliser 
+         * 
+         * 
+         */
+
+        ContactRepository repo;
+
+        [TestInitialize]
+        public void Setup()
         {
+            repo = new ContactRepository();
+        }
+
+        [TestMethod]
+        [TestCategory("ContactRepository Unit Test")]
+        public void a_GetAll_Test()
+        {
+            Assert.AreEqual(0, repo.GetAll().Count);
+        }
+
+        [TestMethod]
+        [TestCategory("ContactRepository Unit Test")]
+        public void b_Insert_Test()
+        {
+            //Arrange
+            Contact c = new Contact { Id = 1, Name = "Dawan" };
+            int tailleAvantInsertion = repo.GetAll().Count;
+
+            //Act
+            repo.Insert(c);
+            int tailleApresInsertion = repo.GetAll().Count;
+
+            //Assert
+            Assert.AreEqual(tailleApresInsertion, tailleAvantInsertion + 1);
+        }
+
+        [TestMethod]
+        [TestCategory("ContactRepository Unit Test")]
+        public void c_GetById_Test()
+        {
+            //Arrange
+            int id = 1;
+
+            //Act
+            Contact c = repo.GetById(id);
+
+            //Assert
+            Assert.AreEqual("Dawan", c.Name);
+        }
+
+        [TestMethod]
+        [TestCategory("ContactRepository Unit Test")]
+        [ExpectedException(typeof(Exception))]
+        public void d_GetById_IdNotExist_ReturnException()
+        {
+            //Arrange
+            int id = 1500;
+
+            //Act
+            repo.GetById(id);
+
+        }
+
+        [TestMethod]
+        [TestCategory("ContactRepository Unit Test")]
+        public void e_Update_Test()
+        {
+            //Arrange
+            int id = 1;
+            Contact c = repo.GetById(1);
+            c.Name = "new name";
+
+            //Act
+            repo.Update(c);
+            Contact contactUpdated = repo.GetById(id);
+
+            //Assert
+            Assert.AreEqual("new name", contactUpdated.Name);
+
+        }
+
+        [TestMethod]
+        [TestCategory("ContactRepository Unit Test")]
+        public void f_Delete_Test()
+        {
+            //Arrange
+            int id = 1;
+            int tailleAvantSuppression = repo.GetAll().Count;
+            //Act
+            repo.Delete(id);
+            int tailleApresSuppression = repo.GetAll().Count;
+
+            //Assert
+            Assert.AreEqual(tailleApresSuppression, tailleAvantSuppression - 1);
+
+        }
+
+        [TestMethod]
+        [TestCategory("ContactRepository Unit Test")]
+        [ExpectedException(typeof(Exception))]
+        public void e_Delete_IdNotExist_ReturnException()
+        {
+            //Arrange
+            int id = 1000;
+
+            //Act
+            repo.Delete(id);
+
         }
     }
 }
---
+++ EntityFR.Test1/Services/ContactServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using AutoFixture;
+using EntityFR.Models;
+using EntityFR.Repositories;
+using EntityFR.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace EntityFR.Test1.Services
+{
+    [TestClass]
+    public class ContactServiceTests
+    {
+        /*
+         * Le service dépend complètement du Repository. A priori les tests unitaires pour le Service
+         * ne sont pas possibles.
+         * Pour mettre en place des tests unitaires pour le Service, on doit mocker le Repository
+         * en utilisant un Framework de Mocking
+         * Framework Moq (.net) - Mockito (java)......
+         * Moq: Framework pour mocker des objets
+         * Fixture: Framework pour mocker des données
+         * 
+         * 
+         */
+
+        ContactService service;
+        Mock<IContactRepository> mockRepo;
+        Fixture fixture;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockRepo = new Mock<IContactRepository>();
+            service = new ContactService(mockRepo.Object);
+            fixture = new Fixture();
+        }
+
+        [TestMethod]
+        [TestCategory("ContactService Unit Test")]
+        public void GetAll_Test()
+        {
+            //List<Contact> contacts = (List<Contact>)fixture.CreateMany<Contact>(5);
+
+            //Arrange
+            List<Contact> lst = new List<Contact>();
+            lst.Add(new Contact());
+            lst.Add(new Contact());
+
+            mockRepo.Setup(r => r.GetAll()).Returns(lst); //Simulation du fonction du repository
+
+            //Act
+            List<Contact> result = service.GetAll();
+
+            //Assert
+            Assert.AreEqual(2, result.Count);
+
+        }
+
+        [TestMethod]
+        [TestCategory("ContactService Unit Test")]
+        public void GetById_Test()
+        {
+
+            //Arrange
+            Contact c = new Contact { Id = 1, Name = "Test" };
+            mockRepo.Setup(r => r.GetById(1)).Returns(c); //Simulation du fonction du repository
+
+            //Act
+            Contact result = service.GetById(1);
+
+            //Assert
+            Assert.AreEqual("Test", result.Name);
+
+        }
+
+        [TestMethod]
+        [TestCategory("ContactService Unit Test")]
+        public void Insert_Test()
+        {
+
+            //Arrange
+            Contact c = new Contact { Id = 1, Name = "Test" };
+            mockRepo.Setup(r => r.Insert(c)).Callback(() => Console.WriteLine("Contact inséré....")); //Simulation du fonction du repository
+
+            //Act
+            service.Insert(c);
+
+            //Il suffit de vérifier si la méthode Insert du Repo est appelée.
+
+        }
+    }
+}
